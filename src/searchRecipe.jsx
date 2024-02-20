fc5eeea4da2eb6372bd0b32462af0dc706e64c8c
Kiya-11 <mgeletu@yahoo.com>
2024-02-20 15:32:53 -0800

    I've added search box and category section

---
+++ src/searchRecipe.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import './searchRecipe.css';
+import searchIcon from './assets/images/search-icon.svg';
+
+function SearchRecipe() {
+    return (
+        <div className="search-container">
+            <form>
+                <input type="text" placeholder="Search for a recipe" />
+                <button><img src={searchIcon} alt="search icon" /></button>
+            </form>
+        </div>
+    );
+}
+
+export default SearchRecipe;    
