c53c6f93994aeffb8ffb066e3c1b6f771b3d23c9
kiya-11 <mgeletu@yahoo.com>
2024-02-19 19:15:22 -0800

    added navigation bar

---
+++ src/navigation.jsx
@@ -0,0 +1,60 @@
+import './navStyle.css';
+
+function Navigation() {
+    return (
+        <nav>
+            <div className="logo">KITCHENWHISPER</div>
+            <div className="nav-list">
+
+
+                <div className="list-container">
+
+                    <button className='nav-list-btn'>Meals</button>
+                    <div className="dropdown-content">
+                        <a href="#">Breakfast</a>
+                        <a href="#">Lunch</a>
+                        <a href="#">Dinner</a>
+                        <a href="#">Snacks</a>
+                    </div>
+                </div>
+
+                <div className="list-container">
+                    <button className='nav-list-btn'>Cuisine</button>
+                    <div className="dropdown-content">
+                        <a href="#">Breakfast</a>
+                        <a href="#">Lunch</a>
+                        <a href="#">Dinner</a>
+                        <a href="#">Snacks</a>
+                    </div>
+
+                </div>
+
+                <div className="list-container">
+                    <button className='nav-list-btn'>Ingredients</button>
+                    <div className="dropdown-content">
+                        <a href="#">Breakfast</a>
+                        <a href="#">Lunch</a>
+                        <a href="#">Dinner</a>
+                        <a href="#">Snacks</a>
+                    </div>
+                </div>
+
+                <div className="list-container">
+                    <button className='nav-list-btn'>Diets</button>
+                    <div className="dropdown-content">
+                        <a href="#">Breakfast</a>
+                        <a href="#">Lunch</a>
+                        <a href="#">Dinner</a>
+                        <a href="#">Snacks</a>
+                    </div>
+                </div>
+            </div>
+
+            <div className="signup-container">
+                <button className="signup">Sign Up</button>
+            </div>
+        </nav>
+    );
+}
+
+export default Navigation;
